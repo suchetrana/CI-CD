bdf305a96a0577e9c739ef5869fc85581f6fe7ab
suchetrana <suchetrana27@gmail.com>
2026-04-23 11:58:22 +0530

    check caching

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ let x=10;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+app.get('/x', (req, res) => { 
+  res.send(`The value of x is: ${x}`);
+});
 app.listen(3000, () => {
   console.log('Server is running on port 3000: http://localhost:3000');
 });
